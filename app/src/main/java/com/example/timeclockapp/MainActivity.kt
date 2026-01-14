5ad069b223f63f06224e5c18a5cda0bc5de8cee9
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2026-01-14 17:33:30 +0600

    fixsss

--- app/src/main/java/com/example/timeclockapp/MainActivity.kt
+++ app/src/main/java/com/example/timeclockapp/MainActivity.kt
@@ -28,9 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                ) {
                     ClockApp()
-                }
             }
         }
     }
